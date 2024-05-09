c5c1e98ad1beca2be1496690671304eddfe02554
Guillaume-prog <76855158+Guillaume-prog@users.noreply.github.com>
2024-05-09 07:29:39 +0100

    Created base project structure

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+    outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            eza
+            fd
+
+            rust-bin.stable.latest.default
+          ];
+
+          shellHook = ''
+            alias ls=eza
+            alias find=fd
+
+            clear
+            export PS1="\n\[\033[1;32m\][rust-env]\[\033[0m\]$ ";
+          '';
+        };
+      }
+    );
+}
