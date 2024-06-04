8645da01f01a494aee46af5ba43bbb4934534dd0
Guillaume-prog <76855158+Guillaume-prog@users.noreply.github.com>
2024-06-04 22:49:45 +0100

    Added diesel as a dependency

--- flake.nix
+++ flake.nix
@@ -18,6 +18,9 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+
+        db-location = "$PWD/.database";
+        db-name = "mydb";
       in
       with pkgs;
       {
@@ -28,14 +31,34 @@
             eza
             fd
 
+            pre-commit
             rust-bin.stable.latest.default
+            diesel-cli
+
+            # Database libraries
+            postgresql
           ];
+
+          installPhase = ''
+            
+          '';
 
           shellHook = ''
             alias ls=eza
             alias find=fd
+            
+            # manage postgres
+            db=${db-location}/${db-name}
+            mkdir -p ${db-location}/tmp
+            
+            if [[ ! -d "$db" ]]; then
+              initdb -D $db
+            fi
 
-            clear
+            pg_ctl -D $db -l ${db-location}/logfile -o "--unix_socket_directories='${db-location}/tmp'" start
+            trap "pg_ctl -D $db stop" EXIT
+
+            # clear
             export PS1="\n\[\033[1;32m\][rust-env]\[\033[0m\]$ ";
           '';
         };
